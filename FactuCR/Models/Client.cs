f636f2f107fb69d4953a1aa68b7d96959c0e024c
joshurarojas <joshua.rojas@ucrso.info>
2018-10-31 12:37:21 -0600

    f

--- FactuCR/Models/Client.cs
+++ FactuCR/Models/Client.cs
@@ -6,10 +6,7 @@ using System.Threading.Tasks;
 namespace FactuCR.Models
 {
     
-    public class Client
+    public class Client 
     {
-       
-
-        public string name { get => name; set => name = value; }
-    }
+           }
 }
---
+++ FactuCR/Models/Entity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FactuCR.Models
+{
+    public class Entity
+    {
+        public string Id_Entity { get; set; }
+        public string Name { get; set; }
+        public string Type_Id { get; set; }
+
+        public string Email { get; set; }
+    }
+}
